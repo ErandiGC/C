0cd1f0e8d57db2ff0225ccd6bf522c2e25a5b483
ErandiGC <erandigc5@gmail.com>
2020-11-08 02:12:28 -0600

    Switch arreglo
    Hacer que el usuario ingrese los valores que se operarán
    
    Cambio de variables enteras a flotantes
    
    Agregar opciones de Mulltiplicación, División, Potencia, Raíz cuadrada

--- control/switchcase.c
+++ control/switchcase.c
@@ -1,23 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 int main()
 {
 	int opcion;
-	printf("Elige una opción:");
+	float a, b, c;
+	
+	printf("\nIngrese el primer numero:");
+	scanf("%f", &a);
+	printf("\nIngrese el segundo numero:");
+	scanf("%f", &b);
+	system("cls");
+	
+	printf("\nElige una opcion:\n");
 	printf("\t 1.- Suma:\n");
 	printf("\t 2.- Resta:\n");
-	printf("\t 6.- Salir:\n");
+	printf("\t 3.- Multiplicacion\n");
+	printf("\t 4.- Division\n");
+	printf("\t 5.- Potencia\n");
+	printf("\t 6.- Raiz cuadrada\n");
+	printf("\t 7.- Salir:\n");
 	scanf("%d",&opcion);
+	system("cls");
+	
+
 	switch(opcion)
 	{
 	case 1: 
-		c= a +b ;
+		c= a + b ;
+		printf("La suma es: %0.2f", c);
 		break;
-	case 2: 
+	case 2:
+		c= a - b ;
+		printf("La resta es: %0.2f", c);
+		break;
+	case 3: 
+		c= a * b ;
+		printf("La multiplicacion es: %0.2f", c);
+		break;
+	case 4:
+		c= a / b ;
+		printf("La division de %0.2f / %0.2f es: %0.2f", a, b, c);
+		c= b / a ;
+		printf("\nLa division de %0.2f / %0.2f es: %0.2f", b, a, c);
+		break;
+	case 5: 
+		c= pow(a,b);
+		printf("La potencia de %0.2f a la %0.2f es: %0.2f", a, b, c);
+		c= pow(b,a) ;
+		printf("\nLa potencia de %0.2f a la %0.2f es: %0.2f", b, a, c);
+		break;
+	case 6:
+	    c= sqrt(a);
+		printf("La raiz cuadrada de %0.2f es: %0.2f", a, c);
+		c= sqrt(b) ;
+		printf("\nLa raiz cuadrada de %0.2f es: %0.2f", b, c);
 		break;
 	default:
-		printf("No está puesta correctamente la unidad\n");
+		printf("\nNo esta puesta correctamente la opcion\n");
 		exit(0);
 	}
-	printf("\nLas %f pulgadas son %f centímetros\n", pul, cm);
+	printf("\nFin");
+	return 0;
 } 
